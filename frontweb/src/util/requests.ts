eb1b3b76e0c54dc1e2a1a58767ee485d22764752
guiosouza <guilhermedeoliveiradesouza@gmail.com>
2022-12-09 19:13:29 -0300

    Authorized request

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,57 +1,71 @@
-
 // Pega uma variável de ambiente;
 // Acessar ambiente onde a aplicação roda em .REACT_APP_BACKEND_URL;
 // Acessar ambiente onde a aplicação roda em .REACT_APP_BACKEND_URL;
 // Caso .REACT_APP_BACKEND_URL não esteja definida, irá acessar o padrão 'http://localhost:8080';
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs';
 
 type LoginResponse = {
-    access_token: string,
-    token_type: string,
-    expires_in: number,
-    scope: string,
-    userFirstName: string,
-    userId: number
-}
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  scope: string;
+  userFirstName: string;
+  userId: number;
+};
 
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+export const BASE_URL =
+  process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 
 const tokenKey = 'authData';
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
 
-const basicHeader = () => 'Basic ' + window.btoa(CLIENT_ID + ":" + CLIENT_SECRET);
+const basicHeader = () =>
+  'Basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET);
 
 type LoginData = {
-    username: string;
-    password: string;
-}
+  username: string;
+  password: string;
+};
 
-export const requestBackendLogin = (loginData : LoginData) => {
-    
-    const headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization: basicHeader(), 
-    }
-    
-    const data = qs.stringify ({
-        ...loginData,
-        grant_type: 'password'
-    });
+export const requestBackendLogin = (loginData: LoginData) => {
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    Authorization: basicHeader(),
+  };
 
-    return axios({method: 'POST', baseURL: BASE_URL, url: '/oauth/token', data, headers})
-}
+  const data = qs.stringify({
+    ...loginData,
+    grant_type: 'password',
+  });
 
-export const saveAuthData = (obj : LoginResponse) => {
-    localStorage.setItem(tokenKey, JSON.stringify(obj));
-}
+  return axios({
+    method: 'POST',
+    baseURL: BASE_URL,
+    url: '/oauth/token',
+    data,
+    headers,
+  });
+};
+
+export const requestBackend = (config: AxiosRequestConfig) => {
+  const headers = config.withCredentials
+    ? {
+        ...config.headers,
+        Authorization: 'Bearer ' + getAuthData().access_token,
+      }
+    : config.headers;
+
+  return axios({ ...config, baseURL: BASE_URL, headers });
+};
+
+export const saveAuthData = (obj: LoginResponse) => {
+  localStorage.setItem(tokenKey, JSON.stringify(obj));
+};
 
 export const getAuthData = () => {
-    const str = localStorage.getItem(tokenKey) ?? "{}";
-    return JSON.parse(str)  as LoginResponse;
-}
-
-
-
+  const str = localStorage.getItem(tokenKey) ?? '{}';
+  return JSON.parse(str) as LoginResponse;
+};
