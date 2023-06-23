a1a39c8ac460eca398d5f39ff0fdf2bb3a602471
guiosouza <guilhermedeoliveiradesouza@gmail.com>
2023-06-23 15:41:59 -0300

    REFACTOR, token, Role

--- frontweb/src/util/__tests__/formatters.spec.ts
+++ frontweb/src/util/__tests__/formatters.spec.ts
@@ -1,12 +1,20 @@
 import { formatPrice } from "util/formatters";
 
-test('formatPrice should format number pt-BR when given 10.1', () => {
-    // ARRANGE
-    const value = 10.1;
+describe('formatPrice for positive numbers', () => {
+    test('formatPrice should format number pt-BR when given 10.1', () => {
+        // ARRANGE
+        const value = 10.2;
+        // ACT
+        const result = formatPrice(value)
+        // ASSERT
+        expect(result).toEqual("10,20");
+    });
 
-    // ACT
-    const result = formatPrice(value)
+    test('formatPrice should format number pt-BR when given 10.1', () => {
+        const value = 0.1;
+        const result = formatPrice(value)
+        expect(result).toEqual("0,10");
+    });
 
-    // ASSERT
-    expect(result).toEqual("10,10");
 });
+
