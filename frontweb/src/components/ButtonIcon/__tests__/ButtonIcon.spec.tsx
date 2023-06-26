f00bf2261190c615200e30b538b6cc9b34b61e8b
guiosouza <guilhermedeoliveiradesouza@gmail.com>
2023-06-26 12:10:01 -0300

    ButtonIcon test

---
+++ frontweb/src/components/ButtonIcon/__tests__/ButtonIcon.spec.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom';
+import ButtonIcon from "..";
+
+
+test('ButtonIcon should render button with given text', () => {
+    // Arrange 
+    const MockedText = "Fazer login";
+
+    // Act (to render de component)
+    render(
+        <ButtonIcon text={MockedText} />
+    );
+
+    screen.debug();
+
+    // Assert - Using queries to find the element
+    expect(screen.getByText(MockedText)).toBeInTheDocument();
+});
