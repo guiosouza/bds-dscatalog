c1756d32a968347b18aa1d852b0d0682f0030b9a
guiosouza <guilhermedeoliveiradesouza@gmail.com>
2023-06-27 15:51:54 -0300

    PrivateRoute fix

--- frontweb/src/components/PrivateRoute/index.tsx
+++ frontweb/src/components/PrivateRoute/index.tsx
@@ -23,7 +23,7 @@ const PrivateRoute = ({ children, path, roles = [] }: Props) => {
         ) : !hasAnyRoles(roles) ? (
           <Redirect to="/admin/products" />
         ) : (
-          children
+          <>{children}</>
         )
       }
     />
